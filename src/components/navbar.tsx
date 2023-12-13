c3f959576b20098dd4c7f2077b61fadcd951fe52
YUVRAJ JWALA <yuvrajjwala@192.168.1.6>
2023-12-14 01:14:00 +0530

    feat: hiding logo in desktop

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 const Navbar = ({ setCardType, cardType }: any) => {
   return (
     <div className="w-full bg-gray-900 border-b border-gray-800 p-6 pl-4 md:pl-10 pr-4 md:pr-10 h-[70px] flex items-center justify-between sticky top-0 z-10">
-      <span>
+      <span className="md:hidden">
         <Image src={"/logo.svg"} height={50} width={50} alt="Aggregator AI" />
       </span>
       <h1>

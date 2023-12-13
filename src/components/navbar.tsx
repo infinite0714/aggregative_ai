abb789f7036fb78e695c5007cdaa26893f0344f2
IntelMin <intelmin0915@gmail.com>
2023-12-13 12:08:44 -0500

    remove goodmorning

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,7 +4,11 @@ import { Button } from "./ui/button";
 const Navbar = ({ setCardType, cardType }: any) => {
   return (
     <div className="w-full bg-gray-900 border-b border-gray-800 p-6 pl-10 pr-10 h-[70px] flex items-center justify-between sticky top-0 z-10">
-      <h1>Good Morning!</h1>
+      <h1>
+        {
+          cardType
+        }
+      </h1>
       {cardType !== "" ? (
         <Button
           onClick={() => setCardType("")}
